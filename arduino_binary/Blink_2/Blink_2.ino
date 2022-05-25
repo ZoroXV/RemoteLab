5ca7c36f95fe1019cfd1cf07b6267fc8ae30cea9
Vincent Muscedere <vincent.muscedere@epita.fr>
2022-05-25 12:54:31 +0200

    test: Add 2 binary for Arduino Uno
    These arduino simply blink 3 led
      Blink_1.ino: blink 3 leds (1 blue, 1 white, 1 red)
      Blink_2.ino: blink 3 leds (1 yellow, 1 white, 1 red)

---
+++ arduino_binary/Blink_2/Blink_2.ino
@@ -0,0 +1,31 @@
+const int blueLedPin = 2;
+const int yellowLedPin = 3;
+const int whiteLedPin = 4;
+const int redLedPin = 5;
+
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(blueLedPin, OUTPUT);
+  pinMode(yellowLedPin, OUTPUT);
+  pinMode(whiteLedPin, OUTPUT);
+  pinMode(redLedPin, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(yellowLedPin, HIGH);
+  
+  delay(100);
+  
+  digitalWrite(yellowLedPin, LOW);
+  digitalWrite(whiteLedPin, HIGH);
+  
+  delay(100);
+  
+  digitalWrite(whiteLedPin, LOW);
+  digitalWrite(redLedPin, HIGH);
+  
+  delay(100);
+  
+  digitalWrite(redLedPin, LOW);
+}
