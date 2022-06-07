655e3d0b950adb88cc73fa2d79b62c4c17eeabc9
Victor LE CORRE <victor.le-corre@epita.fr>
2022-06-07 17:05:41 +0200

    feat: UploadModule: Add shell cmd execution in UploadArduino()

--- src/main.go
+++ src/main.go
@@ -7,5 +7,6 @@ import (
 func main() {
 	upload.UploadInit()
 
-	upload.UploadArduino()
+	upload.UploadArduino("COM3", "arduino:avr:uno",
+		"/home/victor/Documents/RemoteLab/controllers/arduino/blink_1/blink_1.ino.with_bootloader.bin")
 }
--- src/upload/upload.go
+++ src/upload/upload.go
@@ -2,6 +2,8 @@ package upload
 
 import (
 	"fmt"
+	"log"
+	"os/exec"
 )
 
 func UploadInit() {
@@ -9,6 +11,14 @@ func UploadInit() {
 	fmt.Println("Load Configuration...")
 }
 
-func UploadArduino() {
-	fmt.Println("arduino-cli upload -p <port> --fqbn <typeDeCarte> --input-file <binaire>")
+func UploadArduino(port string, cardType string, inputFile string) {
+	cmd := "arduino-cli upload -p " + port + " --fqbn " + cardType + " --input-file " + inputFile
+
+	out, err := exec.Command(cmd).Output()
+	if err != nil {
+		log.Fatal("%s", err)
+	}
+	fmt.Println("Program executed")
+	output := string(out[:])
+	fmt.Println(output)
 }
