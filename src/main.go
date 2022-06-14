4ac90c82b00d865d09205159e7b235d25060acd0
Etienne Chanudet <etienne.chanudet@epita.fr>
2022-06-14 16:16:30 +0200

    feat: link upload and server

--- src/main.go
+++ src/main.go
@@ -1,37 +1,33 @@
 package main
 
 import (
-	"log"
-	"os"
+    "log"
+    "os"
 
-	"remotelab/server"
-	"remotelab/server/rest"
-	//"remotelab/upload"
+    "remotelab/server"
+    "remotelab/server/rest"
+    "remotelab/upload"
 )
 
 var (
-	DEFAULT_CONFIG_FILE string = "./config.json"
+    DEFAULT_CONFIG_FILE string = "./config.json"
 )
 
 func main() {
-	configFile := DEFAULT_CONFIG_FILE
+    configFile := DEFAULT_CONFIG_FILE
 
-	if len(os.Args) == 2 {
-		configFile = os.Args[1]
-	} else if len(os.Args) > 2 {
-		log.Fatalf("[MAIN][ERR] Too much arguments.\n Use: %s [<config_file>]\n", os.Args[0])
-	}
+    if len(os.Args) == 2 {
+        configFile = os.Args[1]
+    } else if len(os.Args) > 2 {
+        log.Fatalf("[MAIN][ERR] Too much arguments.\n Use: %s [<config_file>]\n", os.Args[0])
+    }
 
-	servers := server.CreateServers(configFile)
+    upload.UploadInit()
+    servers := server.CreateServers(configFile)
 
-	for _, serv := range servers {
-		if serv.Protocol == server.REST {
-			rest.RunREST(serv)
-		}
-	}
-
-	//upload.UploadInit()
-
-	//upload.UploadArduino("COM3", "arduino:avr:uno",
-	//	"/home/victor/Documents/RemoteLab/controllers/arduino/blink_1/blink_1.ino.with_bootloader.bin")
+    for _, serv := range servers {
+        if serv.Protocol == server.REST {
+            rest.RunREST(serv)
+        }
+    }
 }
--- src/upload/upload.go
+++ src/upload/upload.go
@@ -1,29 +1,33 @@
 package upload
 
 import (
-	"fmt"
-	"log"
-	"os/exec"
+    "fmt"
+    "log"
+    "os/exec"
 )
 
 func UploadInit() {
-	fmt.Println("Init Upload Module")
-	fmt.Println("Load Configuration...")
+    log.Printf("[UPLOAD] Init upload module...\n")
+    log.Printf("[UPLOAD] Load configuration...\n")
 }
 
-func UploadArduino(port string, cardType string, inputFile string) {
-	cmd := exec.Command(
-		"arduino-cli",
-		"upload",
-		"-p", port,
-		"--fqbn", cardType,
-		"--input-file", inputFile)
+func UploadArduino(port string, cardType string, inputFile string) error {
+    cmd := exec.Command(
+        "arduino-cli",
+        "upload",
+        "-p", port,
+        "--fqbn", cardType,
+        "--input-file", inputFile)
 
-	out, err := cmd.Output()
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("Upload Sucessful")
-	output := string(out[:])
-	fmt.Println(output)
+    out, err := cmd.Output()
+    if err != nil {
+        log.Printf("[UPLOAD][ERR] Fail to upload '%s' on card '%s', port '%s'.\n\t%v\n", inputFile, cardType, port, err)
+        return err
+    }
+
+    log.Printf("[UPLOAD] Upload successful\n")
+    output := string(out[:])
+    fmt.Println(output)
+
+    return nil
 }
