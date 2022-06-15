035f5f0fc3b52948d4614298d8eb45f541c33894
Victor LE CORRE <victor.le-corre@epita.fr>
2022-06-15 18:09:41 +0200

    feat: Add check on os.Args to call CreateDefaultConfig()

--- src/main.go
+++ src/main.go
@@ -1,38 +1,40 @@
 package main
 
 import (
-    "log"
-    "os"
-    "sync"
+	"log"
+	"os"
+	"sync"
 
-    "remotelab/server"
-    "remotelab/server/rest"
-    "remotelab/upload"
+	"remotelab/server"
+	"remotelab/server/rest"
+	"remotelab/upload"
 )
 
 var (
-    DEFAULT_CONFIG_FILE string = "./config.json"
+	DEFAULT_CONFIG_FILE string = "./config.json"
 )
 
 func main() {
-    configFile := DEFAULT_CONFIG_FILE
+	configFile := DEFAULT_CONFIG_FILE
 
-    if len(os.Args) == 2 {
-        configFile = os.Args[1]
-    } else if len(os.Args) > 2 {
-        log.Fatalf("[MAIN][ERR] Too much arguments.\n Use: %s [<config_file>]\n", os.Args[0])
-    }
+	if len(os.Args) == 1 {
+		server.CreateDefaultConfig(configFile)
+	} else if len(os.Args) == 2 {
+		configFile = os.Args[1]
+	} else if len(os.Args) > 2 {
+		log.Fatalf("[MAIN][ERR] Too much arguments.\n Use: %s [<config_file>]\n", os.Args[0])
+	}
 
-    wg := new(sync.WaitGroup)
-    upload.UploadInit()
-    servers := server.CreateServers(configFile)
+	wg := new(sync.WaitGroup)
+	upload.UploadInit()
+	servers := server.CreateServers(configFile)
 
-    for _, serv := range servers {
-        if serv.Protocol == server.REST {
-            wg.Add(1)
-            go rest.RunREST(serv, wg)
-        }
-    }
+	for _, serv := range servers {
+		if serv.Protocol == server.REST {
+			wg.Add(1)
+			go rest.RunREST(serv, wg)
+		}
+	}
 
-    wg.Wait()
+	wg.Wait()
 }
