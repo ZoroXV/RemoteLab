5a22c750b731d9abe156dee3356f42035b2bdc8e
Etienne Chanudet <etienne.chanudet@epita.fr>
2022-07-11 16:32:46 +0200

    feat: fix arduino-cli for RPI

--- src/upload/upload.go
+++ src/upload/upload.go
@@ -3,22 +3,35 @@ package upload
 import (
     "log"
     "os/exec"
+    "os"
 )
 
 func UploadInit() {
     log.Printf("[UPLOAD] Init upload module...\n")
+    cmd := exec.Command(
+	"/home/remotelab/bin/arduino-cli",
+	"core",
+	"install",
+	"arduino:avr")
+
+    cmd.Run()
     log.Printf("[UPLOAD] Load configuration...\n")
 }
 
 func UploadArduino(port string, cardType string, inputFile string) error {
     cmd := exec.Command(
-        "arduino-cli",
+        "/home/remotelab/bin/arduino-cli",
         "upload",
         "-p", port,
         "--fqbn", cardType,
         "--input-file", inputFile)
 
-    err := cmd.Run()
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+
+	err := cmd.Run()
+
     if err != nil {
         log.Printf("[UPLOAD][ERR] Fail to upload '%s' on card '%s', port '%s'.\n\t%v\n", inputFile, cardType, port, err)
         return err
