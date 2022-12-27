002f057f1ac4cc9305c5cca80cbc988681bdb6d4
Etienne Chanudet <etienne.chanudet@epita.fr>
2022-12-27 14:32:39 +0100

    feat(monitor): add function to read lookup table

--- src/upload/lut.go
+++ src/upload/lut.go
@@ -1,5 +1,9 @@
 package upload
 
+import (
+	"errors"
+)
+
 type VendorProduct struct {
 	Vendor	uint16
 	Product	uint16
@@ -11,3 +15,13 @@ var (
 		VendorProduct{0x2341, 0x0043}: "arduino:avr:uno",
 	}
 )
+
+func GetFqbn(VendorID uint16, ProductID uint16) (string, error) {
+	val, exist := LutVendorProductFqbn[VendorProduct{VendorID, ProductID}]
+
+	if exist {
+		return val, nil
+	} else {
+		return "", errors.New("Tuple VendorID ProductID is not in the LookUp Table.")
+	}
+}
