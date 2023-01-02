51a8f25e6ed33fbfcc1eee42b59250c8153ab245
Etienne Chanudet <etienne.chanudet@epita.fr>
2023-01-02 12:20:49 +0100

    feat(install_script): make install script more verbose

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
 # Manage rpot privileges
 if ! [ $(id -u) = 0 ]; then
-   echo "[ERROR] The script need to be run as root." >&2
+   echo "The script need to be run as root." >&2
    exit 1
 fi
 
@@ -19,15 +19,30 @@ BIN_DIR=/usr/bin
 SERVICE_DIR=/etc/systemd/system
 
 # Copy installation files to installation location
+echo -n "Create folders... "
 sudo -u $REAL_USER mkdir -p $INSTALL_DIR
+echo "OK"
+echo -n "Copy files... "
 sudo -u $REAL_USER cp docker-compose.yml $INSTALL_DIR
 sudo -u $REAL_USER cp Dockerfile $INSTALL_DIR
 sudo -u $REAL_USER cp -r src/ $INSTALL_DIR
 cp daemon/launch.sh $BIN_DIR/remotelab.sh
 sed "s~WORKING_DIR~$SUDO_HOME/.remotelab~g" daemon/remotelab.service > $SERVICE_DIR/remotelab.service
+echo "OK"
 
 # Install 
+echo -n "Build docker image... "
 cd $INSTALL_DIR
-sudo -u $REAL_USER docker compose build
+sudo -u $REAL_USER docker compose build &>/dev/null
+RET_VAL=$?
+if [ $RET_VAL -ne 0 ]; then
+    echo "ERROR"
+    exit $RET_VAL
+fi
+echo "OK"
+echo -n "Enable systemctl service... "
 chmod +x $BIN_DIR/remotelab.sh
 systemctl enable --now remotelab.service
+echo "OK"
+
+echo "Installation successful"
