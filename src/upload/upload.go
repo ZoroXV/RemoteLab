c6eeaff859d78fbf328dc938d98d336b02a3588e
Etienne Chanudet <etienne.chanudet@epita.fr>
2023-01-11 16:21:53 +0100

    feat(stm32_compatibility): add uploadSTM() function

--- src/upload/upload.go
+++ src/upload/upload.go
@@ -3,6 +3,7 @@ package upload
 import (
     "log"
     "os/exec"
+    "strconv"
 )
 
 func UploadInit() {
@@ -20,11 +21,36 @@ func UploadArduino(port string, cardType string, inputFile string) error {
 
     err := cmd.Run()
     if err != nil {
-        log.Printf("[UPLOAD][ERR] Fail to upload '%s' on card '%s', port '%s'.\n\t%v\n", inputFile, cardType, port, err)
+        log.Printf("[UPLOAD][ARDUINO][ERR] Fail to upload '%s' on card '%s', port '%s'.\n\t%v\n", inputFile, cardType, port, err)
         return err
     }
 
-    log.Printf("[UPLOAD] Upload successful\n")
+    log.Printf("[UPLOAD][ARDUINO] Upload successful\n")
+
+    return nil
+}
+
+func UploadSTM(port string, serial string, inputFile string, startAddress string) error {
+    _, err := strconv.ParseInt(startAddress, 16, 64)
+
+    if err != nil {
+        log.Printf("[UPLOAD][STM32][ERR] Invalid start address '%s'.\n\t%v\n", startAddress, err)
+        return err
+    }
+
+    cmd := exec.Command(
+        "st-flash",
+        "--serial", serial,
+        "write", inputFile,
+        startAddress)
+
+    err = cmd.Run()
+    if err != nil {
+        log.Printf("[UPLOAD][STM32][ERR] Fail to upload '%s' on card 'STM32', serial '%s', port '%s'.\n\t%v\n", inputFile, serial, port, err)
+        return err
+    }
+
+    log.Printf("[UPLOAD][STM32] Upload successful\n")
 
     return nil
 }
