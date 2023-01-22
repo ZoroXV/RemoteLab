5bb6d9c18d90c104c5efd321f7faee0405b03524
Victor LE CORRE <victor.le-corre@epita.fr>
2023-01-22 16:39:21 +0100

    feat(frontend): Handle /command/list_controllers request on backend to pre-fill upload command form

--- src/frontend/app.py
+++ src/frontend/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, url_for, redirect
 import requests
+import json
 
 app = Flask(__name__)
 raspberrypi_server_ip = 'RPI_IP'
@@ -8,18 +9,60 @@ raspberrypi_server_address = 'http://' + raspberrypi_server_ip + ':' + raspberry
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    data_raw = """
+    {
+        "status": "OK",
+        "message": "",
+        "data": [
+            {
+                "vendor_name": "Arduino SA",
+                "product_name": "Uno",
+                "serial_number": "",
+                "port": "/dev/ttyUSB0",
+                "fqbn": [
+                    "arduino:avr:mega",
+                    "arduino:avr:uno"
+                ]
+            },
+            {
+                "vendor_name": "STMicroelectronics",
+                "product_name": "ST Link",
+                "serial_number": "020000041FF8E3",
+                "port": "",
+                "fqbn": []
+            }
+        ]
+    }
+    """
+
+    data = json.loads(data_raw)
+    return render_template('index.html', data=data['data'])
+
+@app.route('/choose_controller', methods=["POST"])
+def choose_controller():
+    is_update_list = request.form.get('update')
+    if is_update_list:
+        return redirect(url_for('index'), code=302)
+
+    controller_raw = request.form.get('controller')
+
+    if controller_raw is None:
+        raise BaseException
+
+    controller = json.loads(controller_raw.replace("'", '"'))
+    return render_template('upload_command.html', controller=controller)
 
 @app.route('/upload_command', methods=["POST"])
 def upload_command():
-    data_form = request.form.to_dict(flat=False)
     data = {
-        'port': data_form['port'][0],
-        'fqbn': data_form['fqbn'][0],
-        'filename': data_form['filename'][0]
+        'port': request.form.get('port'),
+        'fqbn': request.form.get('fqbn'),
+        'serial_number': request.form.get('serial_number'),
+        'start_address': request.form.get('start_address'),
+        'filename': request.form.get('filename'),
     }
     print(data)
-    response = requests.post(raspberrypi_server_address +'/command/upload', json=data)
+    requests.post(raspberrypi_server_address +'/command/upload', json=data)
     return redirect(url_for('index'), code=302)
 
 @app.route('/upload_binary', methods=["POST"])
@@ -31,7 +74,7 @@ def upload_binary():
 
     data = {'name': uploaded_file.filename}
     files = {'file': open(uploaded_file.filename, 'rb')}
-    response = requests.post(raspberrypi_server_address + '/command/uploadfile', data=data, files=files)
+    requests.post(raspberrypi_server_address + '/command/uploadfile', data=data, files=files)
     return redirect(url_for('index'), code=302)
 
 if __name__ == '__main__':
